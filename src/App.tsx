04e16880c3896500414f149681eb00f5ffa8b21b
no92one <linus.lindroth.92@gmail.com>
2026-01-23 12:04:22 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import type PokemonCard from "./interfaces/pokemonCard.ts"
+
+
+export default function App() {
+  const [pokemonCards, setPokemonCards] = useState<PokemonCard[]>([])
+
+  async function getPokemonCardsData(){
+    const response = await fetch("/pokemonCards.json")
+    const result = await response.json()
+    
+    setPokemonCards(result)
+  }
+
+  useEffect(() => {
+    getPokemonCardsData()
+  }, [])
+
+  return <>
+  <h1>Pokmon Butiken</h1>
+
+    {pokemonCards.map( (card, index) => <section key={index}>
+      <p>{card.name}</p>
+      <img src={card.images.small} alt="No image" />
+    </section>)}
+  </>
+}
---
+++ src/interfaces/pokemonCard.ts
@@ -0,0 +1,6 @@
+import type Images from "./images.ts";
+
+export default interface PokemonCard {
+    name: string;
+    images: Images;
+}
